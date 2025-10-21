dfb11d0b792e5987936c361f2b41f0e661cac23e
Bereket Habtu <37217500+berekethabtu1@users.noreply.github.com>
2025-10-21 01:11:13 -0500

    Update resumes.tsx

--- src/components/resumes.tsx
+++ src/components/resumes.tsx
@@ -29,7 +29,7 @@ const Resume: React.FC = () => {
                     </a>{" "}
                     |
                     <a
-                        href="https://github.com/berekethabtu1"
+                        href="https://github.com/berekethabtu13"
                         target="_blank"
                         rel="noopener noreferrer"
                     >
